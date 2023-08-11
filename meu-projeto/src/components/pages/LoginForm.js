96d4ef1caae2fe10b26e60abbc941ab7555f593d
manoelabms <manoelabusatto04@gmail.com>
2023-08-11 06:32:43 -0300

    basedados

--- meu-projeto/src/components/pages/LoginForm.js
+++ meu-projeto/src/components/pages/LoginForm.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from './LoginForm.module.css';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
   const navigate = useNavigate();
+
   const [credentials, setCredentials] = useState({
     username: '',
+    email: '',
     password: ''
   });
 
@@ -18,28 +19,32 @@ const LoginForm = () => {
     });
   };
 
-const isEmployee = (emailAdress) => {
-    return emailAdress.endsWith('@jj.com');
-};
+  const isEmployee = (emailAddress) => {
+    return emailAddress.endsWith('@jj.com');
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log('Submitted credentials:', credentials);
-  
+
     try {
       const response = await fetch('http://localhost:5000/users');
       const users = await response.json();
-  
-      const matchedUser = users.find(user => user.username === credentials.username && user.password === credentials.password);
-  
+
+      const matchedUser = users.find(user => 
+        user.username === credentials.username && 
+        user.email === credentials.email && 
+        user.password === credentials.password
+      );
+
       if (matchedUser) {
-        if (isEmployee(credentials.username)) {
+        if (isEmployee(credentials.email)) {
           navigate("/task");
         } else {
           navigate("/portal");
         }
       } else {
-        alert('Invalid email or password');
+        alert('Invalid credentials');
       }
     } catch (error) {
       console.error('There was an error:', error);
@@ -48,36 +53,46 @@ const isEmployee = (emailAdress) => {
 
   return (
     <div className={styles.fpageBackground}>
-        <div className={styles.fcontainer}>
+      <div className={styles.fcontainer}>
         <h2 className={styles.fheader}>Entrar</h2>
         <form onSubmit={handleSubmit}>
-            <div className={styles.ffield}>
+          <div className={styles.ffield}>
             <label className={styles.flabel}>Nome de usuário:</label>
             <input
-                type="text"
-                name="username"
-                value={credentials.username}
-                onChange={handleInputChange}
-                className={styles.finput}
+              type="text"
+              name="username"
+              value={credentials.username}
+              onChange={handleInputChange}
+              className={styles.finput}
             />
-            </div>
-            <div className={styles.ffield}>
+          </div>
+          <div className={styles.ffield}>
+            <label className={styles.flabel}>E-mail:</label>
+            <input
+              type="email"
+              name="email"
+              value={credentials.email}
+              onChange={handleInputChange}
+              className={styles.finput}
+            />
+          </div>
+          <div className={styles.ffield}>
             <label className={styles.flabel}>Senha:</label>
             <input
-                type="password"
-                name="password"
-                value={credentials.password}
-                onChange={handleInputChange}
-                className={styles.finput}
+              type="password"
+              name="password"
+              value={credentials.password}
+              onChange={handleInputChange}
+              className={styles.finput}
             />
-            </div>
-            <button type="submit" className={styles.fbutton}>Entrar</button>
-            <div className={styles.signupPrompt}>
-                Não possui uma conta? 
-                <Link to="/signup" className={styles.signupLink}>Criar uma conta</Link>
-            </div>
+          </div>
+          <button type="submit" className={styles.fbutton}>Entrar</button>
+          <div className={styles.signupPrompt}>
+            Não possui uma conta? 
+            <Link to="/signup" className={styles.signupLink}>Criar uma conta</Link>
+          </div>
         </form>
-        </div>
+      </div>
     </div>
   );
 }
